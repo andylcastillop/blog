f5b1ad995e612f02a0f64d126bd9d95141b98dc0
Andy Castillo <andy.castillopalma@outlook.com>
2020-05-29 08:31:44 -0500

    Agregar Artículo

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -5,24 +5,48 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Category;
 use App\Tag;
+use App\Article;
+use App\Image;
+use Laracast\Flash\Flash;
 
 class ArticlesController extends Controller
 {
     public function index(Request $request)
     {
-        //        
+        return view('admin.articles.index');   
     }
     
     public function create()
     {
-        $tags = Tag::orderBy('name', 'ASC')->get();
-        $categories = Category::orderBy('name', 'ASC')->get();
+        $tags = Tag::orderBy('name', 'ASC')->pluck('name', 'id');
+        $categories = Category::orderBy('name', 'ASC')->pluck('name', 'id');
         return view('admin.articles.create')->with('categories', $categories)->with('tags', $tags);
     }
 
     public function store(Request $request)
     {
-        //        
+        //Manipulación de imágenes
+        
+        if ($request->file('image')) {
+            $file = $request ->file('image');
+            $name = 'innovbec_' . time() . '.' . $file->getClientOriginalExtension();
+            $path = public_path() . '/img/articles/';
+            $file->move($path, $name);
+        }
+        
+        $article = new Article($request->all());
+        $article->user_id = \Auth::user()->id;
+        $article->save();
+
+        $article->tags()->sync($request->tags);
+
+        $image = new Image();
+        $image->name = $name;
+        $image->article()->associate($article);
+        $image->save();
+
+        flash('Registro exitoso del artículo '.$article->title)->success()->important();
+        return redirect()->route('articles.index');
     }
 
     public function edit($id)
--- resources/views/admin/articles/create.blade.php
+++ resources/views/admin/articles/create.blade.php
@@ -4,11 +4,28 @@
 
 @section('content')
 
-    {!! Form::open(['route' => 'articles.store', 'method' => 'POST']) !!}
+    {!! Form::open(['route' => 'articles.store', 'method' => 'POST', 'files' => 'true']) !!}
         <div class="form-group">
-            {!! Form::label('name', 'Nombre') !!}
-            {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Nombre del Tag', 'required']) !!}
+            {!! Form::label('title', 'Título') !!}
+            {!! Form::text('title', null, ['class' => 'form-control', 'placeholder' => 'Nombre del Artículo', 'required']) !!}
         </div>
+        <div class="form-group">
+            {!! Form::label('category_id', 'Categoría') !!}
+            {!! Form::select('category_id', $categories, null, ['class' => 'form-control', 'placeholder' => 'Seleccione una opción', 'required']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('content', 'Contenido') !!}
+            {!! Form::textarea('content', null, ['class' => 'form-control', 'placeholder' => 'Información del artículo', 'required']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('tags', 'Tags') !!}
+            {!! Form::select('tags[]', $tags, null, ['class' => 'form-control', 'multiple', 'required']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('image', 'Imagen') !!}
+            {!! Form::file('image') !!}
+        </div>
+        
         <div class="form-group">
             {!! Form::submit('Registrar', ['class' => 'btn btn-primary']) !!}
         </div>
